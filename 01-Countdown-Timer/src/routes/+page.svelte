b05501288cc001e8d77ddc506371040a8ae68c61
Szymon W. <23wilk.szymon@gmail.com>
2024-02-13 04:58:14 +0100

    uytr

--- 01-Countdown-Timer/src/routes/+page.svelte
+++ 01-Countdown-Timer/src/routes/+page.svelte
@@ -4,9 +4,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </svelte:head>
-<script lang="ts">
+<script lang="ts" type="module">
     import moment from 'moment';
-    
+    import { modeStore } from '$lib/store.js';
+
     let day: number = moment().date();
     let month: number = moment().month() + 1;
     let year: number = moment().year();
@@ -14,12 +15,12 @@
     let minutes: number = moment().minutes();
     let seconds: number = moment().seconds();
 
-    let mode: string = "days-hours";
-    let timezone: string = "GMT+1";
-    
-    $: console.log(mode);
-    $: console.log(day, month, year, hour, minutes, seconds);
+    let mode: string = "days";
 
+    let timezone: string = "1";
+    let date: string = `${year}-${month}-${day}`;
+
+    $: modeStore.set(mode);
     function addClassOpen(e: Event) {
         let currentTarget = e.currentTarget as HTMLSelectElement;
         if (currentTarget.parentElement && currentTarget.parentElement.classList.contains('open')) {
@@ -34,7 +35,22 @@
             currentTarget.parentElement.classList.remove('open');
         }
     }
-
+    function createDate() {
+        if (mode === "weeks") {
+            date = `${year}-${month}-${day}T${hour + Number(timezone)}:${minutes}:${seconds}`;
+        } else if (mode === "days") {
+            date = `${day}`;
+        } else {
+            date = `${day}-${hour + Number(timezone)}:${minutes}:${seconds}`;
+        }
+        console.log(date);
+    }
+    function handleSubmit(e: Event) {
+        e.preventDefault();
+        createDate();
+        window.location.href = `./${date}/${timezone}/${mode}`;
+    }
+    //store doesn't export the mode as it should 
 </script>
 <form class="wrapper">
     <h1>Podaj datę do odliczania</h1>
@@ -130,9 +146,36 @@
                 id="timezone-input"
                 on:click={addClassOpen}
                 on:blur={removeClassOpen}
+                bind:value={timezone}
                 >
-                <option value="GMT+1">GMT+1 (UK, Ireland and Portugal)</option>
-            </select>
+                    <option value="-12">GMT-12 (Baker Island)</option>
+                    <option value="-11">GMT-11 (American Samoa)</option>
+                    <option value="-10">GMT-10 (Hawaii)</option>
+                    <option value="-9">GMT-9 (Alaska)</option>
+                    <option value="-8">GMT-8 (Pacific Time)</option>
+                    <option value="-7">GMT-7 (Mountain Time)</option>
+                    <option value="-6">GMT-6 (Central Time)</option>
+                    <option value="-5">GMT-5 (Eastern Time)</option>
+                    <option value="-4">GMT-4 (Atlantic Time)</option>
+                    <option value="-3">GMT-3 (Argentina, Brazil, Chile)</option>
+                    <option value="-2">GMT-2 (Falkland Islands, South Georgia and the South Sandwich Islands)</option>
+                    <option value="-1">GMT-1 (Cape Verde, Azores)</option>
+                    <option value="0">GMT+0 (Greenwich Mean Time)</option>
+                    <option value="1">GMT+1 (UK, Ireland, Germany)</option>
+                    <option value="2">GMT+2 (South Africa, Egypt, Greece)</option>
+                    <option value="3">GMT+3 (Russia, Saudi Arabia, Turkey)</option>
+                    <option value="4">GMT+4 (Azerbaijan, United Arab Emirates, Armenia)</option>
+                    <option value="5">GMT+5 (Pakistan, Uzbekistan, Kazakhstan)</option>
+                    <option value="6">GMT+6 (Kazakhstan, Bangladesh, Myanmar)</option>
+                    <option value="7">GMT+7 (Thailand, Vietnam, Indonesia)</option>
+                    <option value="8">GMT+8 (China, Australia, Malaysia)</option>
+                    <option value="9">GMT+9 (Japan, South Korea, North Korea)</option>
+                    <option value="10">GMT+10 (Australia, Papua New Guinea, Guam)</option>
+                    <option value="11">GMT+11 (Solomon Islands, Vanuatu, New Caledonia)</option>
+                    <option value="12">GMT+12 (New Zealand, Fiji, Marshall Islands)</option>
+                    <option value="13">GMT+13 (Tonga, Samoa, Kiribati)</option>
+                    <option value="14">GMT+14 (Kiribati, Line Islands)</option>
+                </select>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18"><path fill="#1E293B" fill-rule="evenodd" d="M3.97 6.22a.75.75 0 0 1 1.06 0L9 10.19l3.97-3.97a.75.75 0 1 1 1.06 1.06l-4.5 4.5a.75.75 0 0 1-1.06 0l-4.5-4.5a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/></svg>
         </div>
     </div>
@@ -175,7 +218,12 @@
             </label>
         </div>
     </div>
-    <button type="submit" class="submit-btn">Wygeneruj czas</button>
+    <button 
+        type="submit" 
+        class="submit-btn"
+        on:click={handleSubmit}
+        >Wygeneruj czas
+    </button>
 </form>
 
 
---
+++ 01-Countdown-Timer/src/routes/[date]/[timezone]/[mode]/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts" type="module">
+	import { modeStore } from '$lib/store.js';
+</script>
+
+<div class="wrapper">
+	<h1>Pozostały czas</h1>
+    <p>{$modeStore}</p>
+	<div class="timer-container">
+		{#if $modeStore === "days"}
+			<div class="days">
+				<span>Dni</span>
+				<p>15</p>
+			</div>
+		{:else if $modeStore === "days-hours"}
+			<div class="days">
+				<span>Dni</span>
+				<p>15</p>
+			</div>
+			<div class="hours">
+				<span>Godzina</span>
+				<p>15</p>
+			</div>
+			<div class="minutes">
+				<span>Minuty</span>
+				<p>15</p>
+			</div>
+			<div class="seconds">
+				<span>Sekundy</span>
+				<p>15</p>
+			</div>
+		{:else}
+			<div class="weeks">
+				<span>Tydzień</span>
+				<p>15</p>
+			</div>
+			<div class="days">
+				<span>Dni</span>
+				<p>15</p>
+			</div>
+			<div class="hours">
+				<span>Godzina</span>
+				<p>15</p>
+			</div>
+			<div class="minutes">
+				<span>Minuty</span>
+				<p>15</p>
+			</div>
+			<div class="seconds">
+				<span>Sekundy</span>
+				<p>15</p>
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style lang="scss">
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+		h1 {
+			color: var(--text);
+			font-size: 20px;
+			text-align: left;
+			font-weight: 600;
+		}
+	}
+</style>
