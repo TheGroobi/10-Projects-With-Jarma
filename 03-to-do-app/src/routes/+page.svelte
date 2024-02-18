6fcde34cf3ca2d70984e2892bd7a4f6fb82b78d8
Szymon W. <23wilk.szymon@gmail.com>
2024-02-18 16:09:15 +0100

    Finished front

--- 03-to-do-app/src/routes/+page.svelte
+++ 03-to-do-app/src/routes/+page.svelte
@@ -3,76 +3,115 @@
 </svelte:head>
 
 <script lang="ts">
-    import { dndzone } from 'svelte-dnd-action';
+    import { flip, } from 'svelte/animate';
+    import { fade, scale } from 'svelte/transition';
 
 let taskInputValue: string = '';
 let validation: boolean = true;
+let maxTask: boolean = true;
+let taskList = [{value: 'aplikacja do zarządzania zadaniami w svelcie (ToDo List)', checked: false, id: 0}, {value: '1', checked: false, id: 1}, {value: '2', checked: false, id: 2}, {value: '3', checked: false, id: 3}, {value: '4', checked: false, id: 4}, {value: '5', checked: false, id: 5}, {value: '6', checked: false, id: 6}, {value: '7', checked: false, id: 7}, {value: '8', checked: false, id: 8}, {value: '9', checked: false, id: 9}, {value: '10', checked: false, id: 10}];
 
+let taskDragging: any;
 function capitalizeFirstLetter(input: string) {
     if (input.length) {
         taskInputValue = input[0].toUpperCase() + input.slice(1);
     }
 }
-let taskList = [{value: 'aplikacja do zarządzania zadaniami w svelcie (ToDo List)', checked: false, id: 0}, {value: '1', checked: false, id: 1}, {value: '2', checked: false, id: 2}, {value: '3', checked: false, id: 3}, {value: '4', checked: false, id: 4}, {value: '5', checked: false, id: 5}, {value: '6', checked: false, id: 6}, {value: '7', checked: false, id: 7}, {value: '8', checked: false, id: 8}, {value: '9', checked: false, id: 9}, {value: '10', checked: false, id: 10}];
 
 function addTask() {
-    if (taskInputValue === '') {
-        validation = false;
-        return;
+    if (taskList.length <= 15) {
+        maxTask = true;
+        if (taskInputValue === '') {
+            validation = false;
+            return;
+        } else {
+            validation = true;
+            capitalizeFirstLetter(taskInputValue);
+            const task = { value: taskInputValue, checked: false, id: taskList.length + 1}
+            taskList = [...taskList, task];
+            taskInputValue = '';
+        }
     } else {
-        validation = true;
-        capitalizeFirstLetter(taskInputValue);
-        const task = { value: taskInputValue, checked: false, id: taskList.length + 1}
-        taskList = [...taskList, task];
-        taskInputValue = '';
+        maxTask = false;
+        return;
     }
 }
 
 function removeTask(i: number) {
+    if (taskList.length <= 16) {
+        maxTask = true;
+    }
     taskList.splice(i, 1);
     taskList = [...taskList];
 }
 
 function moveDownTask(i: number) {
-    const removedTask = taskList.splice(i, 1);
-    taskList = [...taskList, removedTask[0]];
+    if (taskList[i].checked) {
+        const movedTask = taskList.splice(i, 1);
+        taskList = [...taskList, movedTask[0]];
+    } else {
+        const movedTask = taskList.splice(i, 1);
+        taskList = [...movedTask, ...taskList];    
+    }
 }
-
-function handleConsider(e: any) {
-    console.log('consider', e.detail);
-    taskList = e.detail.items;
+function taskListDragging (e: DragEvent) {
+    const id = (e.target as HTMLElement).getAttribute('id');
+    const task = taskList.find(task => task.id === Number(id));
+    if (task && !task.checked) {
+        taskDragging = task;
+    } else {
+        taskDragging = null;
+    }
 }
-function handleFinalize(e: any) {
-    console.log('finalize', e.detail);
-    taskList = e.detail.items;
+function assignedDrop (e: DragEvent) {
+    if (!taskDragging) return;
+    const dropTarget = (e.target as HTMLElement);
+    if (dropTarget.nodeName === 'LI') {
+        const id = dropTarget.getAttribute('id');
+        const dropTagetTask = taskList.find(task => task.id === Number(id));
+        if (dropTagetTask && !dropTagetTask.checked) {
+            const dropTargetIndex = taskList.indexOf(dropTagetTask);
+            taskList.splice(taskList.indexOf(taskDragging), 1);
+            taskList = [...taskList.splice(dropTargetIndex, 0, taskDragging), ...taskList];
+        }
+    }
 }
-
 </script>
 
 <main class="wrapper">
     <h1>Lista zadań</h1>
     <div 
-        class="task-container"
-        use:dndzone={{ items: taskList }}
-        on:consider={handleConsider}
-        on:finalize={handleFinalize}
+        on:drop={assignedDrop}
+        on:dragover={(e) => e.preventDefault()}
+        role="list"
     >
-            {#each taskList as task, i}
-                <div class="task">
+        <ul class="task-container">
+            {#each taskList as task, i (task.id)}
+                <li class="task"
+                    on:drag={taskListDragging}
+                    draggable="true"
+                    id="{task.id.toString()}"
+                    out:fade
+                    animate:flip={{delay: 200, duration: 1500 - (i * 50)}}
+                    class:completed-task={task.checked}
+                >
                     <input
                         type="checkbox"
                         name="task-checkbox"
                         bind:checked={task.checked}
                         on:change={() => moveDownTask(i)}
-                        />
-                        <svg class="check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14"><path fill="#fff" fill-rule="evenodd" d="M12.08 3.088a.583.583 0 0 1 0 .824L5.661 10.33a.583.583 0 0 1-.824 0L1.92 7.412a.583.583 0 0 1 .825-.824L5.25 9.092l6.004-6.004a.583.583 0 0 1 .825 0Z" clip-rule="evenodd"/></svg>
+                    />
+                    {#if task.checked}
+                        <svg transition:scale class="check" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14"><path fill="#fff" fill-rule="evenodd" d="M12.08 3.088a.583.583 0 0 1 0 .824L5.661 10.33a.583.583 0 0 1-.824 0L1.92 7.412a.583.583 0 0 1 .825-.824L5.25 9.092l6.004-6.004a.583.583 0 0 1 .825 0Z" clip-rule="evenodd"/></svg>
+                    {/if}
                     <span class:completed={task.checked}>{task.value}</span>
                     <button type="button" on:click={() => removeTask(i)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
                     </button>
-                </div>
-            {/each}
-        </div>
+                </li>
+                {/each}
+        </ul>
+    </div>
         <form>
             <input 
             bind:value={taskInputValue}
@@ -85,7 +124,10 @@ function handleFinalize(e: any) {
             </button>
         </form>
         {#if validation === false}
-            <p class="error">Zadanie musi mieć treść</p>
+            <p class="error" transition:scale>Zadanie musi mieć treść</p>
+        {/if}
+        {#if maxTask === false}
+            <p class="error" transition:scale>Możesz dodać maksymalnie 15 zadań</p>
         {/if}
 </main>
 <style lang="scss">
@@ -96,6 +138,7 @@ function handleFinalize(e: any) {
     $checkbox-green: #84CC16;
     $background: #F1F5F9;
     $white: #fff;
+    $whiteFade: rgba(248, 250, 252, 1);
 
     @mixin input {
         display: flex;
@@ -142,6 +185,7 @@ function handleFinalize(e: any) {
             gap: 0.5rem;
             width: 26.25rem;
             position: relative;
+            padding-inline-start: 0;
             .task {
                 display: flex;
                 gap: 0.75rem;
@@ -197,7 +241,7 @@ function handleFinalize(e: any) {
                     right: 1rem;
                     &:hover {
                         svg {
-                            color: $checkbox-green;
+                            color: red;
                         }
                     }
                     svg {
@@ -205,9 +249,13 @@ function handleFinalize(e: any) {
                         color: $text;
                         width: 1rem;
                         height: 1rem;
+                        transition: all 0.1s ease-in-out;
                     }
                 }
             }
+        }
+        .completed-task {
+            background-color: $whiteFade !important;
         }
             form {
                 position: relative;
@@ -242,6 +290,7 @@ function handleFinalize(e: any) {
                         color: $text;
                         width: 1rem;
                         height: 1rem;
+                        transition: all 0.1s ease-in-out;
                     }
                 }
             }
