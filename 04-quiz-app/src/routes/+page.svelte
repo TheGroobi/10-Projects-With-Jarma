5fcf6fce09712195220690e89b9f6ce87a3a4600
Szymon W. <23wilk.szymon@gmail.com>
2024-02-24 00:35:10 +0100

    Initial

--- 04-quiz-app/src/routes/+page.svelte
+++ 04-quiz-app/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <div class="w-max py-8 px-6 bg-white border border-stroke rounded">
 	<a
 		href="/quiz"
-		class="bg-emerald-500 text-bold rounded px-4 py-2 font-bold text-white"
-		>Rozpocznij Quiz</a>
+		class="bg-emerald-500 text-bold rounded px-4 py-2 font-bold text-white">Rozpocznij Quiz</a>
 </div>
