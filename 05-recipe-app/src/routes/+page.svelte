e4cd5ccb428c41d5f9cd25ea29e53e751879d64d
Szymon W. <23wilk.szymon@gmail.com>
2024-02-26 09:53:29 +0100

    dsfgfd

---
+++ 05-recipe-app/src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import Image from '$lib/assets/Image_placeholder.png';
+
+    let ingredientsArr = ['jaja', 'jak', 'berety'];
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<div class="bg-slate-950 grid place-items-center w-full min-h-svh">
+	<main class="p-8 md:p-16 bg-bg-light flex flex-col gap-8 xl:max-w-7xl lg:max-w-5xl md:max-w-3xl">
+		<form action="" class="sm:gap-8 gap-4 flex flex-col sm:flex-row">
+			<div class="flex relative xl:w-[72rem] lg:w-[60rem] md:w-[44rem]">
+				<input
+					type="text"
+					class="w-full bg-bg-main rounded-lg px-4 py-2 font-medium outline-none text-text-main"
+					placeholder="Wyszukaj przepisu"
+				/>
+				<svg
+					class="absolute right-4 top-3 pointer-events-none"
+					xmlns="http://www.w3.org/2000/svg"
+					width="18"
+					height="18"
+					viewBox="0 0 18 18"
+					fill="none"
+				>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M8.24902 3C5.34953 3 2.99902 5.3505 2.99902 8.25C2.99902 11.1495 5.34953 13.5 8.24902 13.5C11.1485 13.5 13.499 11.1495 13.499 8.25C13.499 5.3505 11.1485 3 8.24902 3ZM1.49902 8.25C1.49902 4.52208 4.5211 1.5 8.24902 1.5C11.9769 1.5 14.999 4.52208 14.999 8.25C14.999 11.9779 11.9769 15 8.24902 15C4.5211 15 1.49902 11.9779 1.49902 8.25Z"
+						fill="#F8FAFC"
+					/>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M11.9936 11.9947C12.2865 11.7018 12.7614 11.7018 13.0543 11.9947L16.2793 15.2197C16.5721 15.5126 16.5721 15.9874 16.2793 16.2803C15.9864 16.5732 15.5115 16.5732 15.2186 16.2803L11.9936 13.0553C11.7007 12.7624 11.7007 12.2876 11.9936 11.9947Z"
+						fill="#F8FAFC"
+					/>
+				</svg>
+			</div>
+			<a
+				href="/"
+				class="bg-orange-500 rounded-lg text-invert font-bold text-base px-8 py-2 text-center"
+				>Powrót</a
+			>
+		</form>
+		<section class="flex flex-col gap-4">
+			<h1 class="text-text-main text-3xl tracking-[-0.01496rem] font-semibold">
+				Title Placeholder
+			</h1>
+			<img src={Image} alt="Placeholder Alt Name" />
+		</section>
+		<section>
+			<p class="px-2 py-1 text-yellow-100 bg-ingredients w-min rounded-lg">Składniki</p>
+            <div>
+                {#each ingredientsArr as ingredient}
+                <Task ingredient={ingredient}/>
+                {/each}
+            </div>
+		</section>
+	</main>
+</div>
--- 05-recipe-app/tailwind.config.js
+++ 05-recipe-app/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'bg-light': '#334155',
+        'bg-main': '#1E293B',
+        'text-main': '#F8FAFC',
+        'text-secondary': '#94A3B8',
+        'text-invert': 'rgba(30, 41, 59, 1)',
+        'ingredients': '#3D3E33',
+      }
+    }
   },
   plugins: []
 };
